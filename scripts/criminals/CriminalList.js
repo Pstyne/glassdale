0b69587936c36f07e39c43139b06115be271e8dd
Andrew Birt <abirt1986@gmail.com>
2021-10-22 13:49:16 -0400

    completed practice Nav Link for Criminals

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,17 +1,32 @@
 import { getCriminals, useCriminals } from "./CriminalDataProvider.js";
 import { Criminal } from "./Criminal.js";
 
-const el = document.querySelector('.criminal-list');
+const el = document.querySelector('.main-content');
+
+document.querySelector("#criminals-nav-link").addEventListener("click", function(){
+  CriminalList();          
+});
 
 export const CriminalList = () => {
+  
   let html = '';
   
   getCriminals()
   .then(() => {
     const criminals = useCriminals();
 
+    html += `
+    <section class="criminals">
+      <h2>Criminals</h2>
+      <div class="convictions-select-container"></div>
+      <div class="criminal-list flex-container">
+      <!-- Begin Criminal List -->
+    `;
+
     criminals.forEach( criminalObj => html += Criminal(criminalObj));
 
-    el.innerHTML = html;
-  });
+    el.innerHTML += html;
+    html += `</div>
+    </section>`
+  });     
 }
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,12 @@ import { OfficerList } from "./officers/OfficerList.js";
 import { CriminalList } from "./criminals/CriminalList.js";
 import { FacilityList } from "./facilities/FacilityList.js";
 
-OfficerList();
-CriminalList();
-FacilityList();
+// OfficerList();
+// FacilityList();
+
+const darkModeButton = document.querySelector('#dark-mode');
+
+darkModeButton.addEventListener("click", function(){
+  const el = document.querySelector('body');
+  el.classList.toggle("dark-background");
+});
